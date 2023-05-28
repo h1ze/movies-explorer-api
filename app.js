3f8b370937f5aea7fec048644cfffed34c933646
h1ze <burgov@mail.ru>
2023-05-28 11:57:08 +0400

    feat: добавить централизованный обработчик ошибок

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const helmet = require('helmet');
 
 const app = express();
 const { PORT, DATABASE } = require('./utils/config');
+const mainRouter = require('./routes');
+const mainErorHandler = require('./middlewares/mainErorHandler');
 
 app.use(express.json());
 
@@ -13,6 +15,10 @@ mongoose.connect(DATABASE, {
 });
 
 app.use(helmet());
+
+app.use(mainRouter);
+
+app.use(mainErorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`);
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const mainRouter = require('express').Router();
+const NotFoundError = require('../errors/not-found-err');
+
+// Импорт роутеров
+const users = require('./users');
+
+mainRouter.use('/users', users);
+mainRouter.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
+
+module.exports = mainRouter;
