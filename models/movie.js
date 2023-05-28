a0130f7f70248790a4568bfb4b4a6a8ef42a8b19
h1ze <burgov@mail.ru>
2023-05-28 10:07:29 +0400

    feat: создать модель Movie

---
+++ models/movie.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, 'Поле "country" должно быть заполнено'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Поле "director" должно быть заполнено'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Поле "duration" должно быть заполнено'],
+  },
+  year: {
+    type: String,
+    required: [true, 'Поле "year" должно быть заполнено'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Поле "description" должно быть заполнено'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Поле "image" должно быть заполнено'],
+    validate: {
+      validator: (value) => {
+        validator.isURL(value);
+      },
+      message: 'Некорректный URL',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: [true, 'Поле "trailerLink" должно быть заполнено'],
+    validate: {
+      validator: (value) => {
+        validator.isURL(value);
+      },
+      message: 'Некорректный URL',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: [true, 'Поле "thumbnail" должно быть заполнено'],
+    validate: {
+      validator: (value) => {
+        validator.isURL(value);
+      },
+      message: 'Некорректный URL',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, 'Поле "owner" должно быть заполнено'],
+  },
+  movieId: {
+    type: Number,
+    required: [true, 'Поле "movieId" должно быть заполнено'],
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Поле "nameRU" должно быть заполнено'],
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'Поле "nameEN" должно быть заполнено'],
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('movie', movieSchema);
