724c314ff90dd72f3a2f5f706eb5ac3deb8bdf60
h1ze <burgov@mail.ru>
2023-05-28 14:16:12 +0400

    fix: исправить экспорт userRouter

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const NotFoundError = require('../errors/not-found-err');
 // Импорт роутеров
 const users = require('./users');
 
-mainRouter.use('/users', users);
+mainRouter.use('/users/me', users);
 mainRouter.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,13 @@ const userRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { getUser, updateUser } = require('../controllers/users');
 
-userRouter.get('/me', getUser);
+userRouter.get('/', getUser);
 
-userRouter.patch('/me', celebrate({
+userRouter.patch('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
   }),
 }), updateUser);
+
+module.exports = userRouter;
