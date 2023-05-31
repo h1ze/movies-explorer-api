dfa393808913a91fbc5901263b0a40002d50ce69
h1ze <burgov@mail.ru>
2023-05-31 16:05:07 +0400

    fix: исправить условие в corsOptions

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -2,7 +2,7 @@ const { ALLOWED_CORS } = require('./constants');
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (ALLOWED_CORS.indexOf(origin) !== -1) {
+    if (ALLOWED_CORS.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
