46f7a4b75536fe52b9f7fa3be8a1a6ad94e01e27
h1ze <burgov@mail.ru>
2023-06-01 09:27:24 +0400

    fix: убрать eslint-disable в auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,6 @@
 const { checkToken } = require('../utils/token');
 const AuthError = require('../errors/auth-err');
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   let payload;
   try {
@@ -22,5 +21,5 @@ module.exports = (req, res, next) => {
 
   req.user = payload; // записываем пейлоуд в объект запроса
 
-  next(); // пропускаем запрос дальше
+  return next(); // пропускаем запрос дальше
 };
